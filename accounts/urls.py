fc4ba57a2316b229b55c79491d3f7f2642eba40d
Jeremy Jones <jeremy@jeremyjjones.xyz>
2024-08-20 20:17:30 -0700

    Updated/added urls, views and templates for accounts app

---
+++ accounts/urls.py
@@ -0,0 +1,12 @@
+"""module contains the URL patterns for the accounts app"""
+
+from django.urls import path
+from . import views
+
+APP_NAME = "accounts"
+
+urlpatterns = [
+    path("register/", views.register_view, name="register"),
+    path("login/", views.login_view, name="login"),
+    path("logout/", views.logout_view, name="logout"),
+]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,59 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import  login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
-# Create your views here.
+# REGISTER VIEW
+def register_view(request):
+    """
+    Handles user registration.
+
+    Args:
+        request (HttpRequest): The current request object.
+
+    Returns:
+        HttpResponse: The rendered registration page or a redirect to the login page.
+    """
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("login")
+    else:
+        form = UserCreationForm()
+    return render(request, "accounts/register.html", {"form": form})
+
+# LOGIN VIEW
+def login_view(request):
+    """
+    Handles user login.
+
+    Args:
+        request (HttpRequest): The current request object.
+
+    Returns:
+        HttpResponse: The rendered login page or a redirect to the file list page.
+    """
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect("file_list")
+    else:
+        form = AuthenticationForm()
+    return render(request, "accounts/login.html", {"form": form})
+
+# LOGOUT VIEW
+def logout_view(request):
+    """
+    Handles user logout.
+
+    Args:
+        request (HttpRequest): The current request object.
+
+    Returns:
+        HttpResponse: A redirect to the login page.
+    """
+    if request.method == "POST":
+        logout(request)
+        return redirect("login")
